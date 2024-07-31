50a1c4a76b439f8253a4906a5d106b79a7f65b35
Venkata Narendra Kumar Yerriboina <venkatanarendrakumar.yerriboina@mtahq.org>
2024-07-31 11:28:34 -0400

    updated flutter version

--- example/android/app/src/main/kotlin/dev/flutterclutter/simple_edge_detection_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.flutterclutter.simple_edge_detection_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/edge_detection.dart
+++ lib/edge_detection.dart
@@ -5,31 +5,29 @@ import 'dart:ui';
 import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
 
-
-class Coordinate extends Struct {
+final class Coordinate extends Struct {
   @Double()
-  double x;
+  external double x;
 
   @Double()
-  double y;
+  external double y;
 
-  factory Coordinate.allocate(double x, double y) =>
-      malloc<Coordinate>().ref
-        ..x = x
-        ..y = y;
+  factory Coordinate.allocate(double x, double y) => malloc<Coordinate>().ref
+    ..x = x
+    ..y = y;
 }
 
-class NativeDetectionResult extends Struct {
-  Pointer<Coordinate> topLeft;
-  Pointer<Coordinate> topRight;
-  Pointer<Coordinate> bottomLeft;
-  Pointer<Coordinate> bottomRight;
+final class NativeDetectionResult extends Struct {
+  external Pointer<Coordinate> topLeft;
+  external Pointer<Coordinate> topRight;
+  external Pointer<Coordinate> bottomLeft;
+  external Pointer<Coordinate> bottomRight;
 
   factory NativeDetectionResult.allocate(
-      Pointer<Coordinate> topLeft,
-      Pointer<Coordinate> topRight,
-      Pointer<Coordinate> bottomLeft,
-      Pointer<Coordinate> bottomRight) =>
+          Pointer<Coordinate> topLeft,
+          Pointer<Coordinate> topRight,
+          Pointer<Coordinate> bottomLeft,
+          Pointer<Coordinate> bottomRight) =>
       malloc<NativeDetectionResult>().ref
         ..topLeft = topLeft
         ..topRight = topRight
@@ -39,10 +37,10 @@ class NativeDetectionResult extends Struct {
 
 class EdgeDetectionResult {
   EdgeDetectionResult({
-    @required this.topLeft,
-    @required this.topRight,
-    @required this.bottomLeft,
-    @required this.bottomRight,
+    required this.topLeft,
+    required this.topRight,
+    required this.bottomLeft,
+    required this.bottomRight,
   });
 
   Offset topLeft;
@@ -52,32 +50,29 @@ class EdgeDetectionResult {
 }
 
 typedef DetectEdgesFunction = Pointer<NativeDetectionResult> Function(
-  Pointer<Utf8> imagePath
-);
+    Pointer<Utf8> imagePath);
 
 typedef process_image_function = Int8 Function(
-  Pointer<Utf8> imagePath,
-  Double topLeftX,
-  Double topLeftY,
-  Double topRightX,
-  Double topRightY,
-  Double bottomLeftX,
-  Double bottomLeftY,
-  Double bottomRightX,
-  Double bottomRightY
-);
+    Pointer<Utf8> imagePath,
+    Double topLeftX,
+    Double topLeftY,
+    Double topRightX,
+    Double topRightY,
+    Double bottomLeftX,
+    Double bottomLeftY,
+    Double bottomRightX,
+    Double bottomRightY);
 
 typedef ProcessImageFunction = int Function(
-  Pointer<Utf8> imagePath,
-  double topLeftX,
-  double topLeftY,
-  double topRightX,
-  double topRightY,
-  double bottomLeftX,
-  double bottomLeftY,
-  double bottomRightX,
-  double bottomRightY
-);
+    Pointer<Utf8> imagePath,
+    double topLeftX,
+    double topLeftY,
+    double topRightX,
+    double topRightY,
+    double bottomLeftX,
+    double bottomLeftY,
+    double bottomRightX,
+    double bottomRightY);
 
 // https://github.com/dart-lang/samples/blob/master/ffi/structs/structs.dart
 
@@ -89,43 +84,39 @@ class EdgeDetection {
         .lookup<NativeFunction<DetectEdgesFunction>>("detect_edges")
         .asFunction<DetectEdgesFunction>();
 
-    NativeDetectionResult detectionResult = detectEdges(path.toNativeUtf8()).ref;
+    NativeDetectionResult detectionResult =
+        detectEdges(path.toNativeUtf8()).ref;
 
     return EdgeDetectionResult(
         topLeft: Offset(
-            detectionResult.topLeft.ref.x, detectionResult.topLeft.ref.y
-        ),
+            detectionResult.topLeft.ref.x, detectionResult.topLeft.ref.y),
         topRight: Offset(
-            detectionResult.topRight.ref.x, detectionResult.topRight.ref.y
-        ),
+            detectionResult.topRight.ref.x, detectionResult.topRight.ref.y),
         bottomLeft: Offset(
-            detectionResult.bottomLeft.ref.x, detectionResult.bottomLeft.ref.y
-        ),
-        bottomRight: Offset(
-            detectionResult.bottomRight.ref.x, detectionResult.bottomRight.ref.y
-        )
-    );
+            detectionResult.bottomLeft.ref.x, detectionResult.bottomLeft.ref.y),
+        bottomRight: Offset(detectionResult.bottomRight.ref.x,
+            detectionResult.bottomRight.ref.y));
   }
 
-  static Future<bool> processImage(String path, EdgeDetectionResult result) async {
+  static Future<bool> processImage(
+      String path, EdgeDetectionResult result) async {
     DynamicLibrary nativeEdgeDetection = _getDynamicLibrary();
 
     final processImage = nativeEdgeDetection
         .lookup<NativeFunction<process_image_function>>("process_image")
         .asFunction<ProcessImageFunction>();
 
-
     return processImage(
-        path.toNativeUtf8(),
-        result.topLeft.dx,
-        result.topLeft.dy,
-        result.topRight.dx,
-        result.topRight.dy,
-        result.bottomLeft.dx,
-        result.bottomLeft.dy,
-        result.bottomRight.dx,
-        result.bottomRight.dy
-    ) == 1;
+            path.toNativeUtf8(),
+            result.topLeft.dx,
+            result.topLeft.dy,
+            result.topRight.dx,
+            result.topRight.dy,
+            result.bottomLeft.dx,
+            result.bottomLeft.dy,
+            result.bottomRight.dx,
+            result.bottomRight.dy) ==
+        1;
   }
 
   static DynamicLibrary _getDynamicLibrary() {
